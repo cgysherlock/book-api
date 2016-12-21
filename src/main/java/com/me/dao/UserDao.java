2a1c352e74f34f8a9ce444ad6fc4e198ce924e11
sherlock <251879836@qq.com>
2016-12-21 15:01:24 +0800

    添加关注

--- src/main/java/com/me/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.me.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserDao extends BaseDao{
-
-}
--- src/main/java/com/yhq/controller/ConcernController.java
+++ src/main/java/com/yhq/controller/ConcernController.java
@@ -1,19 +1,19 @@
-package com.me.controller;
-
-import javax.servlet.http.HttpSession;
+package com.yhq.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.me.controller.BaseController;
 import com.me.http.HttpKit;
 import com.me.http.Response;
 import com.me.model.Message;
-import com.me.service.ConcernService;
+import com.yhq.service.ConcernService;
+
 
 @RestController
-@RequestMapping(value = "")
+@RequestMapping(value = "/concern")
 public class ConcernController extends BaseController {
 
 	@Autowired
@@ -23,11 +23,10 @@ public class ConcernController extends BaseController {
 	 * 关注他人
 	 * itsid被关注的人的id
 	 */
-	@RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	public String concernOthers(long itsid, HttpSession session) {
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	public String concernOthers(Long concernerId, Long concernedId) {
 		//得到存在session中的用户id
-		long myid=(int)session.getAttribute("myid");
-		Message message = concernService.concernOthers(myid,itsid);
+		Message message = concernService.concernOthers(concernerId,concernedId);
 		Response response = new Response(message);
 		return HttpKit.toJson(response);
 	}
